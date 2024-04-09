848064aff454e7aae305af2759b2072f4e4703e3
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-04-09 02:33:19 -0300

    hello world works

--- Browser.fs
+++ Browser.fs
@@ -1,10 +1,5 @@
 ﻿open Browser
 
-let mutable count = 0
-
 let div = document.createElement "div"
-div.innerHTML <- "<div>" + count.ToString() + "</div>"
-
-div.innerHTML <- div.innerHTML + $"<button onclick=\"{fun _ -> count + 1}\">click</button>"
-
+div.innerHTML <- "Hello world!"
 document.body.appendChild div |> ignore
--- Browser.fs.js
+++ Browser.fs.js
@@ -1,12 +1,7 @@
-import { int32ToString, createAtom } from "./fable_modules/fable-library-js.4.16.0/Util.js";
-
-export let count = createAtom(0);
 
 export const div = document.createElement("div");
 
-div.innerHTML = (("<div>" + int32ToString(count())) + "</div>");
-
-div.innerHTML = (div.innerHTML + (`<button onclick="${(_arg) => (count() + 1)}">click</button>`));
+div.innerHTML = "Hello world!";
 
 document.body.appendChild(div);
 
